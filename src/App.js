3837152a1012350e726adc8f5e3fa8314f007839
Duracyq <szymon.kojek@wp.pl>
2023-05-09 03:17:17 +0200

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const url = 'https://www.google.com/search?q=cats'\
+
+
+  const shortenUrl = async () => {
+    const response = fetch("https://gotiny.cc/api", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ input: url }),
+    })
+    console.log(response)
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form method='post'>
+        <input id="inputUrl" />
+        <button type="submit" onClick={this.shortenUrl}>SHORTEN</button>
+      </form>
+
     </div>
   );
 }
